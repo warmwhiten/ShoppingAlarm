fe102c572d8923e3601a12d86c40ba777bd87b31
dahee kim <ekekekgml@khu.ac.kr>
2020-06-02 16:30:11 +0900

    Update Loading Screen

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+import Loading from './Loading';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync(); 
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      // Send to API and get weather
+      this.setState({isLoading:false});
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+
+  }
+  componentDidMount(){
+    this.getLocation()
+  }
+  render() {
+    const{ isLoading } = this.state;
+    return isLoading?  <Loading/> : null;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
